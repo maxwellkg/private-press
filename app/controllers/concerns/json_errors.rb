99cc38e699fbe19505dd52479ef4df0dcfedf32c
Maxwell Gove <maxwell.gove@hey.com>
2026-04-19 13:04:35 -0400

    add standard json errors

---
+++ app/controllers/concerns/json_errors.rb
@@ -0,0 +1,20 @@
+module JSONErrors
+  extend ActiveSupport::Concern
+
+  def render_json_error(code, message = nil)
+    render json: { error: { code:, message: } }, status: status_for_code(code)
+  end
+
+  private
+    STATUS_MAPPINGS = {
+      unauthenticated: 401,
+      forbidden: 403,
+      not_found: 404,
+      validation_failed: 422,
+      rate_limited: 429
+    }.freeze
+
+    def status_for_code(code)
+      STATUS_MAPPINGS.fetch(code)
+    end  
+end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,3 +1,4 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular "leaf", "leaves"
+  inflect.acronym "JSON"
 end
