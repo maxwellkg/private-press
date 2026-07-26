f48ab1d91a46388c1a6d3eef365272fd73d93092
Maxwell Gove <maxwell.gove@hey.com>
2026-07-25 20:49:13 -0400

    add API route for books index; rescue from record not found in API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   include Authentication, Authorization, JSONErrors, VersionHeaders
 
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern, unless: -> { request.format.json? }
+
+  private
+    def handle_record_not_found(error)
+      if request.format.json?
+        render_json_error(:not_found, "Not found.")
+      else
+        raise error
+      end
+    end
 end
